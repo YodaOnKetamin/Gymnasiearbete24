93e624f899e087ceb08a759df3d4d8682f0eeb72
YodaOnKetamin <Simon.Ulander.06@gmail.com>
2024-09-19 10:32:37 +0200

    sedrtgh

---
+++ Assets/Scripts/Uppgrades/CPSUpgrades.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class CPSUpgrades : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Manager;
+    CPSManager cPSM;
+    ClickableCoin CC;
+
+    [SerializeField]
+    TMP_Text cost;
+    [SerializeField]
+    TMP_Text uppgradeLevel;
+    [SerializeField]
+    Button button;
+
+    int level;
+    [SerializeField]
+    public float upCost;
+    [SerializeField]
+    public float incCost;
+    public float incPerLevel;
+    int upCostRound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cPSM = Manager.GetComponent<CPSManager>();
+        CC = Manager.GetComponent<ClickableCoin>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CC.CoinCounter < upCostRound)
+        {
+            button.interactable = false;
+        }
+        else
+        {
+            button.interactable = true;
+        }
+        upCostRound = (int)(upCost + 0.5f);
+        uppgradeLevel.text = level.ToString();
+        cost.text = upCostRound.ToString();
+    }
+
+    public void onButtonPress()
+    {
+        if (CC.CoinCounter >= upCostRound)
+        {
+            level++;
+            CC.CoinCounter -= upCostRound;
+            upCost *= incCost;
+        }
+    }
+}
